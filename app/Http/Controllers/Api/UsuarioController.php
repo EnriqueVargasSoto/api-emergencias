b3e29d281c7430c43f8bf79879048c2e188230a5
EnriqueVargasSoto <45204929+EnriqueVargasSoto@users.noreply.github.com>
2022-11-27 14:22:55 -0500

    14.22

--- app/Http/Controllers/Api/UsuarioController.php
+++ app/Http/Controllers/Api/UsuarioController.php
@@ -44,7 +44,7 @@ class UsuarioController extends Controller
 
         $data = $request->json()->all();
 
-        //return $data;
+        return json_encode('200');
 
         $image = $this->getB64Image($data['image']);
 
